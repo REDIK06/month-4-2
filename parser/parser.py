2df5b8492673120982f2595ca7dfce25af66ca36
REDIK06 <ceremuskinroma956@gamil.com>
2024-03-01 18:39:32 +0600

    спарсил сайт

--- parser/parser.py
+++ parser/parser.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as BS
 URL = "https://cinematica.kg"
 
 HEADERS = {
-    "Accept": "*/*",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
 
@@ -30,7 +30,7 @@ def parsing():
     response = get_html(URL)
     if response.status_code == 200:
         cinematica_list_2 = []
-        for page in range(1, 3):  # Проход дважды для страниц 1 и 2
+        for page in range(1, 3):
             response = get_html(f'https://cinematica.kg/movies/', params={'page': page})
             cinematica_list_2.extend(get_data(response.text))
         return cinematica_list_2
